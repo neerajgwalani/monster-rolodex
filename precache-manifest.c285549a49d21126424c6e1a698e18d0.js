24002fb223595fc1980349df095839034999d72e
neeraj gwalani <neerajgwalani29@gmail.com>
2020-09-16 20:12:38 +0530

    Updates

---
+++ precache-manifest.c285549a49d21126424c6e1a698e18d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4767f583b81aecac5693d88633aef165",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "d643ea52426fd631e700",
+    "url": "/monster-rolodex/static/css/main.ef413f52.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d643ea52426fd631e700",
+    "url": "/monster-rolodex/static/js/main.af55a8bf.chunk.js"
+  },
+  {
+    "revision": "d2a9c49a159018779ca4",
+    "url": "/monster-rolodex/static/js/runtime-main.131bb20d.js"
+  }
+]);
